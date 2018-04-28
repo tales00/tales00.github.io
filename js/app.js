b6f95c52f234fa7b58f3afcae33008d78d3e5ac3
tales <tales00@users.noreply.github.com>
2018-04-28 20:11:34 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,1 @@
+"use strict";var vm=new Vue({el:"#app",data:{skills:[{img:"Unofficial_JavaScript_logo_2.svg",text:"JavaScript"},{img:"es6.svg",text:"ES6"},{img:"pwa-logo.png",text:"PWA"},{img:"vue.svg",text:"vue.js"},{img:"jquery.svg",text:"jQuery"},{img:"css3.svg",text:"CSS3"},{img:"rwd.svg",text:"RWD"},{img:"git_icon.svg",text:"git"}]},computed:{},methods:{}});
